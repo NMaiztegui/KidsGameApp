a6b7c30b4a28b0552af5d8716eb96133343886a3
julengalin <galindo.julen@uni.eus>
2025-01-27 10:22:35 +0100

    Ionic proiektua sortuta

---
+++ Ionic_Proiektua/Galdutako_dokumentuaren_bila/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Galdutako_dokumentuaren_bila',
+  webDir: 'www'
+};
+
+export default config;
