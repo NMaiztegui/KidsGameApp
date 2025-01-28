94d46d7305f0ad6013f43f74120ae932506e251e
Naroa Maiztegui <naroamaiztegui@gmail.com>
2025-01-28 13:15:19 +0100

    docs(#24): modeloa eta migrazioak (faktory + seeder) lokalizazioa eta erronka

---
+++ api-laravel-KidsGameApp/app/Models/erronka.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class erronka extends Model
+{
+    /** @use HasFactory<\Database\Factories\ErronkaFactory> */
+    use HasFactory;
+}
---
+++ api-laravel-KidsGameApp/database/migrations/2025_01_28_121225_create_erronkas_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('erronkas', function (Blueprint $table) {
+            $table->id();
+            $table->longText('azalpena');
+            $table->longText('testu_izkutua');
+            $table->string('mapa_irudia_ruta');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('erronkas');
+    }
+};
