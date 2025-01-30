579e4687d7c00a91d9b8d8618feb2498a9dc5550
Naroa Maiztegui <naroamaiztegui@gmail.com>
2025-01-30 19:40:34 +0100

    feat(#59):insert api data into sqlite

--- Aplikazioa/src/app/services/sqlite.service.ts
+++ Aplikazioa/src/app/services/sqlite.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
-
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { SQLitePorter } from '@awesome-cordova-plugins/sqlite-porter/ngx';
+import { SQLite, SQLiteObject } from '@awesome-cordova-plugins/sqlite/ngx';
+import { Platform } from '@ionic/angular';
 @Injectable({
   providedIn: 'root'
 })
 export class SqliteService {
+  private storage!: SQLiteObject;
+  private isDbReady: BehaviorSubject<boolean> = new BehaviorSubject(false);
+  constructor(private sqlite: SQLite, private platform: Platform) { 
 
-  constructor() { }
+    this.platform.ready().then(() => {
+      this.sqlite.create({
+        name: 'kidsappgame.db',
+        location: 'default'
+      }).then((db: SQLiteObject) => {
+        this.storage = db;
+        this.isDbReady.next(true);
+      });
+    });
+  }
+//inser api data to sqlite
+  public insertData(tableName: string, data: any[]) {
+    let columns = Object.keys(data[0]).join(', ');
+    let placeholders = Object.keys(data[0]).map(() => '?').join(', ');
+
+    let queries = data.map(row => {
+      let values = Object.values(row);
+      return this.storage.executeSql(`INSERT INTO ${tableName} (${columns}) VALUES (${placeholders})`, values);
+    });
+
+    return Promise.all(queries);
+  }
+
+  public getDbState() {
+    return this.isDbReady.asObservable();
+  }
 }
