62f01d00bd02e3b6b089c60949e33b2be7c5e2d5
asierferrero <asierferrero09@gmail.com>
2025-01-30 10:04:29 +0100

    feat (#30): Hasierako orriak eta konfigurazioa eginda

--- Aplikazioa/src/app/erronka1/erronka1.page.ts
+++ Aplikazioa/src/app/erronka1/erronka1.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-erronka1',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ErronkaPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   testua = 'Tx_kit_tik Tr_p_ga_anek_ m_ateg_etat_k mu_itu i_an na_z. Ni_e ha_rtzaro_ren zati_ik han_ien_ ing_ru hauet_n eman dut, f_mi_ia eta sen_rrare_in ego_eko. Horre_aiti_, her_i eta ingu_uko se_retu gehi_nak dakizkit. Ib_lbid_an zehar desku_ritzek_ pre_t zaud_te?';
   erantzunZuzena = 'Txikitatik Trapagaraneko meategietatik mugitu izan naiz. Nire haurtzaroaren zatirik handiena inguru hauetan eman dut, familia eta senarrarekin egoteko. Horregaitik, herri eta inguruko sekretu gehienak dakizkit. Ibilbidean zehar deskubritzeko prest zaudete?';
@@ -30,8 +31,8 @@ export class ErronkaPage implements OnInit {
   }
 
   ariketaSubmit() {
-
-  }
+    this.router.navigate(['/mapa'], { queryParams: { erronka: 2 } });
+  }  
   
   ariketaBerregin() {
     this.erantzuna = null;
--- Aplikazioa/src/app/mapa/mapa.page.ts
+++ Aplikazioa/src/app/mapa/mapa.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-mapa',
@@ -8,9 +8,10 @@ import { Router } from '@angular/router';
   standalone: false,
 })
 export class MapaPage implements OnInit {
-  
+  erronkaId: number | null = 1;
+
   private erronkas = [
-    { id: 1, latitud: 43.1794257, longitud: -2.491341 },
+    { id: 1, latitud: 40.1794257, longitud: -2.491341 },
     { id: 2, latitud: 43.1800000, longitud: -2.492000 },
     { id: 3, latitud: 43.1810000, longitud: -2.493000 },
     { id: 4, latitud: 43.1820000, longitud: -2.494000 },
@@ -23,9 +24,14 @@ export class MapaPage implements OnInit {
   private coordenadasUsuario = { latitud: 0, longitud: 0 };
   private margenError = 0.01;
 
-  constructor(private router: Router) {}
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      if (params['erronka']) {
+        this.erronkaId = +params['erronka'];
+      }
+    });
     this.obtenerCoordenadasUsuario();
   }
 
@@ -53,9 +59,9 @@ export class MapaPage implements OnInit {
   }
 
   erronkaHasi() {
-    const erronkaId = this.lekuanBadago();
-    if (erronkaId !== null) {
-      this.router.navigate([`/erronka${erronkaId}`]);
+    const erronkaIdLocalizado = this.lekuanBadago();
+    if (erronkaIdLocalizado === this.erronkaId) {
+      window.location.href = `/erronka${erronkaIdLocalizado}`;
     }
   }
 }
