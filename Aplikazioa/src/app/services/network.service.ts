73a7c09b8e0afb9f3637cab0acc616f5c55e0a03
Naroa Maiztegui <naroamaiztegui@gmail.com>
2025-01-31 10:32:45 +0100

    fix(#29): angular eta node_modules bertsio arazoak konpontzen

--- Aplikazioa/src/app/services/network.service.ts
+++ Aplikazioa/src/app/services/network.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
-
+import { Network } from '@capacitor/network';
 @Injectable({
   providedIn: 'root'
 })
 export class NetworkService {
+  connected = true;
+  constructor() {  this.checkInitialStatus();
 
-  constructor() { }
+    Network.addListener('networkStatusChange', async status => {
+        console.log('Sarearen konexioa aldatu da:', status);
+        this.connected = status.connected;
+      });
+  }
+  async checkInitialStatus() {
+    const status = await Network.getStatus();
+    console.log('Sarearen hasierako egoera:', status);
+    this.connected = status.connected;
+  }
+  getStatus(): boolean {
+    return this.connected;
+  }
 }
