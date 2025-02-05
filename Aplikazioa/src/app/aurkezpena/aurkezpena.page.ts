ce73ac4f281755c90df5c5139f5b146605485bb3
Naroa Maiztegui <naroamaiztegui@gmail.com>
2025-02-05 09:54:02 +0100

    feat(#30): aurkezpen testua poliki poliki erreproduzitu

--- Aplikazioa/src/app/aurkezpena/aurkezpena.page.ts
+++ Aplikazioa/src/app/aurkezpena/aurkezpena.page.ts
@@ -9,6 +9,11 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class AurkezpenaPage implements OnInit {
 
+  fullText: string = 'Dolores herri honen historia eta garrantzia oso ondo ezagutzen  dituen andre bat da. Politikari espainiarra izan zelako da ezaguna batez ere, baina hori alde batera utzita ondo ezagutzen zuen La Arboledako eta inguruko meatzaritzaren historia osoa...';
+  displayedText: string = ''; // Texto que se mostrará progresivamente
+  index: number = 0;
+  speed: number = 55; // Velocidad en ms
+
   erronkaId: number | null = null;  // Se inicializa como null
 
   constructor(private route: ActivatedRoute) {}
@@ -19,6 +24,20 @@ export class AurkezpenaPage implements OnInit {
       this.erronkaId = +params.get('id')!;  // 'id' es el nombre del parámetro en la ruta
       console.log('Erronka ID desde URL:', this.erronkaId);
     });
+
+    this.showText();
+  }
+
+  showText() {
+    const interval = setInterval(() => {
+      if (this.index < this.fullText.length) {
+        this.displayedText += this.fullText[this.index];
+        this.index++;
+      } else {
+        this.index = 0;
+        clearInterval(interval); // Detener cuando termina
+      }
+    }, this.speed);
   }
 }
 
